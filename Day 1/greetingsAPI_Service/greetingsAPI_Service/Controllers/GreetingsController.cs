5c74453c5003dcc7cd8a5a18f72a848212cafc24
Nikhil <nikhilshah.net@hotmail.com>
2023-05-17 16:38:46 +0530

    Day 1

---
+++ Day 1/greetingsAPI_Service/greetingsAPI_Service/Controllers/GreetingsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace greetingsAPI_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("greetings")]
+        public IActionResult Greetings()
+        {
+            return Ok("Hello and Welcome to WebAPI Development");
+        }
+
+        [HttpGet]
+        [Route("greetme/{name}")]
+        public IActionResult Greetings(string name)
+        {
+            return Ok("Hello " + name);
+        }
+
+        [HttpGet]
+        [Route("calculate/{num1}/{num2}")]
+        public IActionResult AddNumbers(int num1, int num2)
+        {
+            int add = num1 + num2;
+            return Ok(add);
+        }
+
+
+
+    }
+}

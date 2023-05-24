eb1b9d82f64f256b4ba70035b6d702420bb0ac9c
Nikhil <nikhilshah.net@hotmail.com>
2023-05-24 16:51:02 +0530

    Day 6

---
+++ Day 6/api_fetchcall/api_fetchcall/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace api_fetchcall.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
